d9cf254588fadab056753829a7ff24167f3fdb3f
fm074 <familiamanuel317@gmail.com>
2026-06-24 00:26:11 +0100

    first commit

--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -1,6 +1,6 @@
 export const environment = {
     appUrl: 'https://app.kursinha.com',
     // appUrl: 'https://app.kursinha.com',
-    // serverUrl: 'https://api.kursinha.com'
-    serverUrl: 'http://localhost:3000'
+    serverUrl: 'https://api.kursinha.com'
+    // serverUrl: 'http://localhost:3000'
 }
